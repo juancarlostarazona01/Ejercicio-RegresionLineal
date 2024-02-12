b205258b0c3a8175bd0c7b686482d04dc915593a
Juan Carlos Tarazona Contreras <157440609+juancarlostarazona01@users.noreply.github.com>
2024-02-12 15:00:09 -0500

    Add files via upload

---
+++ Debug/CMakeFiles/RegresionLineal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RegresionLineal.dir/extoeigen.cpp.o"
+  "CMakeFiles/RegresionLineal.dir/main.cpp.o"
+  "CMakeFiles/RegresionLineal.dir/modellinealregression.cpp.o"
+  "RegresionLineal"
+  "RegresionLineal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RegresionLineal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
